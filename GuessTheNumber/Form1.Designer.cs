3edbf760cba96724edae9bc48d7c12e62186bb9c
UsameYigit <usameyigit1@gmail.com>
2022-01-21 11:25:34 +0300

    Form design prepared.

--- GuessTheNumber/Form1.Designer.cs
+++ GuessTheNumber/Form1.Designer.cs
@@ -29,20 +29,69 @@ namespace GuessTheNumber
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblInformation = new System.Windows.Forms.Label();
+            this.lblGuessResult = new System.Windows.Forms.Label();
+            this.btnGuess = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(130, 111);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // lblInformation
+            // 
+            this.lblInformation.AutoSize = true;
+            this.lblInformation.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblInformation.Location = new System.Drawing.Point(64, 43);
+            this.lblInformation.Name = "lblInformation";
+            this.lblInformation.Size = new System.Drawing.Size(392, 24);
+            this.lblInformation.TabIndex = 1;
+            this.lblInformation.Text = "Please put a number between 1 and 100.";
+            // 
+            // lblGuessResult
+            // 
+            this.lblGuessResult.AutoSize = true;
+            this.lblGuessResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblGuessResult.Location = new System.Drawing.Point(152, 185);
+            this.lblGuessResult.Name = "lblGuessResult";
+            this.lblGuessResult.Size = new System.Drawing.Size(0, 20);
+            this.lblGuessResult.TabIndex = 2;
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Location = new System.Drawing.Point(276, 111);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(75, 23);
+            this.btnGuess.TabIndex = 3;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(517, 307);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.lblGuessResult);
+            this.Controls.Add(this.lblInformation);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblInformation;
+        private System.Windows.Forms.Label lblGuessResult;
+        private System.Windows.Forms.Button btnGuess;
     }
 }
 
